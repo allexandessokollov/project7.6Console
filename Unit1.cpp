a6fa9870d693a7ef12c5d436aabf3fad1f51d1b1
allexandessokollov <64951280+allexandessokollov@users.noreply.github.com>
2020-12-17 13:07:58 +0300

    Update Unit1.cpp
    changed answer out. got rid of magic numbers

--- Unit1.cpp
+++ Unit1.cpp
@@ -11,25 +11,28 @@
 #include <stdlib.h>
 #pragma argsused
 
-int countAnswer(std::string str);
+const int MAX_ANSWER_SIZE = 128,
+MAX_STRING_SIZE = 512,
+MAX_BUFFER_SIZE = 512;
+
+void fingIntegerNumber(char[]);
 bool isNumber(char ch);
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::string str;
+
     int answer;
-    char chArr[512];
+
+    char chArr[MAX_STRING_SIZE];
 
     printf("Enter your string\n");
 
     gets(chArr);
 
-    str = chArr;
+    fingIntegerNumber(chArr);
 
-    countAnswer(str);
-
-    printf("\n\n\nEnter 1 to close application ");
-    char ch[20];
+    printf("\n\n\npress any sign to close application ");
+    char ch[MAX_BUFFER_SIZE];
 
     gets(ch);
 
@@ -41,19 +44,16 @@ int main()
 
 
 
-int countAnswer(std::string str)
+void fingIntegerNumber(char chArr[])
 {
 
 
-    str += " "; 
-    const char *chArr = str.c_str();
+    int counter = 0,strLength = strlen(chArr);
 
-    int counter = 0, tmpCounter = 0, strLength = strlen(chArr);
-
+    char num[MAX_ANSWER_SIZE];
 
 
-    char num[128];
-    for(int i = 0; i < 128; i++)
+    for(int i = 0; i < MAX_ANSWER_SIZE; i++)
     {
         num [i] = ' ';
     }
@@ -78,63 +78,38 @@ int countAnswer(std::string str)
             }
         }
 
-        if( (counter > 0) && (!isNumber(chArr[i])) )
+        if( (counter > 0) && (i == strLength - 1 || !isNumber(chArr[i])))
         {
         
             printf("\n\nyour integer number is:\n");
-            printf(num);
-            return 1;
+
+            for(int i = 0; i < MAX_ANSWER_SIZE; i++)
+            {
+                if(num[i] != ' ')
+                {
+                    printf("%c" ,num[i]);
+                }
+
+            }
+
+            break;
         }
 
     }
 
+    
     if(counter == 0)
     {
         printf("\n\nthere is no integer number\n\n");
     }
 
 
-    return 1;
+    
 }
 
 
 bool isNumber(char ch)
 {
-    switch (ch)
-    {
-        case '0':
-        return true;
-
-        case '1':
-        return true;
-
-        case '2':
-        return true;
-
-        case '3':
-        return true;
-
-        case '4':
-        return true;
-
-        case '5':
-        return true;
-
-        case '6':
-        return true;
-
-        case '7':
-        return true;
-
-        case '8':
-        return true;
-
-        case '9':
-        return true;
-
-        default:
-        return false;
-    }
-}
-//---------------------------------------------------------------------------
+    return ch >= '0'  && ch <= '9';
+}-----------------
  
